fabc2f1c9cb920834cb64e5bffdca3d018c4dff9
alexandrumatt <48669024+alexandrumatt@users.noreply.github.com>
2023-11-27 08:51:56 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-# Verifica daca a primit un singur argument
+# Verifica daca script-ul a primit exact un argument
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <character>"
     exit 1
 fi
 
-# Extrage primul argument
-character="$1"
+character=$1
+counter=0
 
-# Contor pentru propozitiile corecte
-correct_sentences=0
+is_valid_sentence() {
+    local sentence=$1
 
-# Citeste linii de la intrarea standard pana la sfarsitul fisierului
-while IFS= read -r line; do
-    # Verifica daca linia incepe cu o litera mare
-    if [[ $line =~ ^[[:upper:]] ]]; then
-        # Verifica daca linia contine doar caractere valide
-        if [[ $line =~ ^[[:upper:][:lower:][:digit:] ,.!?]*$ ]]; then
-            # Verifica daca linia se termina cu un caracter valid
-            if [[ $line =~ [,.!?]$ ]]; then
-                # Verifica daca nu contine virgula inainte de 'si'
-                if [[ ! $line =~ ,\s+si ]]; then
-                    # Propozitia este corecta
-                    ((correct_sentences++))
+    # Verifica daca propozitia incepe cu litera mare
+    if [[ ${sentence:0:1} =~ [A-Z] ]]; then
+        # Verifica daca propozitia contine doar caractere valide si caracterul dorit
+        if [[ $sentence =~ ^[A-Za-z0-9\ ,.\s!?]+$ && $sentence == *"$character"* ]]; then
+            # Verifica daca propozitia se termina cu ? sau ! sau .
+            if [[ "${sentence: -1}" =~ [?!\.] ]]; then
+                # Verifica daca propozitia nu contine virgula inainte de "si"
+                if [[ $sentence != *", si"* ]]; then
+                    return 0  # Propozitia este corecta
                 fi
             fi
         fi
     fi
+
+    return 1  # Propozitia nu este corecta
+}
+
+# Citeste linii de la intrarea standard pana la EOF
+while IFS= read -r line; do
+    
+    echo $line
+    # Incrementare contor daca propozitia este corecta
+    if is_valid_sentence "$line"; then
+        ((counter++))
+    fi
+    echo $counter
 done
 
-# Afiseaza contorul
-echo $correct_sentences
+# Afiseaza valoarea numerica a contorului
+echo $counter
+
